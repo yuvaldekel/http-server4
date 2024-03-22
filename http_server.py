fa583af2fa4772927c174f55311c94070819e99c
Yuval Dekel <yuvddd05@gmail.com>
2024-03-22 20:47:01 +0200

    add comments

--- http_server.py
+++ http_server.py
@@ -8,9 +8,13 @@ SOCKET_TIMEOUT = 1
 REDIRECT  = {'\\index1.html': '\\index.html'}
 FORBIDDEN = {'\\index3.html'}
 
+
+#check if the request structure is okay
+#if it is separate to the http method and http resource 
+#if the method is post return also the content length header
 def check_request(request):
     content_length = None
-    path = None
+    resource = None
     valid = False
 
     end_line = request.index("\r\n")
@@ -18,16 +22,20 @@ def check_request(request):
     
     if  re.search("^((GET )|(POST ))((\/[a-zA-Z0-9=\-_\.\?&]{0,}){1,})( HTTP\/[1-9\.]+)$", request_line):
         valid = True
-        path = request_line.split(' ')[1]
+        resource = request_line.split(' ')[1]
         method = request_line.split(' ')[0]
         if method == "POST":
             content_length = get_content_length(request)
-    return valid, method, path, content_length
+    return valid, method, resource, content_length
 
+#get http request find the content-length header and return it's value
 def get_content_length(request):
     if "Content-Length: " in request:
         return int(request[request.index("Content-Length: ")+16:request.index("\r\n",request.index("Content-Length:"))])
     
+#get relative path to a file, take the file type and read it
+#return the file length file content and file type by http standard
+#if the file type is not one that we suppose to read return raise exception
 def get_file_data(path):
     file_type = path.split('.')[-1]
     if file_type == 'txt' or file_type == 'html':
@@ -64,65 +72,73 @@ def get_file_data(path):
     else:
         raise ValueError 
 
-def post_image(path, data):
+#get resource take from it the image name and save the image
+def post_image(resource, data):
 
-    if not path.endswith(".jpg") and "file_name" not in path:
+    if not resource.endswith(".jpg") and "file_name" not in resource:
         raise ValueError
-    file_name = path[path.index('file-name=')+10:]
+    file_name = resource[resource.index('file-name=')+10:]
     if os.path.isfile(UPLOADS + file_name):
         raise ValueError
     with open(UPLOADS + file_name, 'wb') as file:
             file.write(data)
             return file_name, len(data)
-    
-def send_image(path):
-    if not path.endswith('.jpg'):
-        path = path + '.jpg'
-    if "image-name=" not in path:
+
+#get resource take from it which image to read read it using get_file_data
+def send_image(resource):
+    if not resource.endswith('.jpg'):
+        resource = resource + '.jpg'
+    if "image-name=" not in resource:
         raise ValueError
-    image_name = "\\uploads\\" + path[path.index('image-name=')+11:]
+    image_name = "\\uploads\\" + resource[resource.index('image-name=')+11:]
     return get_file_data(image_name)
 
-def get_next(path):  
-    if 'num=' not in path:
+#get resource take the number parameter and return number + 1
+def get_next(resource):  
+    if 'num=' not in resource:
         raise ValueError
-    number = int(path[path.index('num=') + 4:])
+    number = int(resource[resource.index('num=') + 4:])
     next = number + 1 
     return next, len(str(next))
 
-def area(path):
-    if 'height=' not in path and "width=" not in path:
+#get resource take the hight and width parameter and calculate area
+def area(resource):
+    if 'height=' not in resource and "width=" not in resource:
         raise ValueError
-    height = int(path[path.index('height=') + 7: path.index('&')])
-    width = int(path[path.index('width=')+6:])
+    height = int(resource[resource.index('height=') + 7: resource.index('&')])
+    width = int(resource[resource.index('width=')+6:])
     if  width * height % 2 == 0:
         value = width * height // 2
     else:
         value = width * height / 2
     return value, len(str(value))
 
-def GET(path):
-    if path.startswith('\\calculate-next?'):
+#if the method is get, check the resource
+#pass to the correct function depend on the resource
+#from the function result create response if the function raised error
+#return http error code depend on the exception
+def GET(resource):
+    if resource.startswith('\\calculate-next?'):
         try:
-            next, length = get_next(path)
+            next, length = get_next(resource)
             return f"HTTP/1.1 200 OK\r\nContent-Length: {length}\r\nContent-Type: text/plain\r\n\r\n{next}".encode()
         except ValueError:
             return "HTTP/1.1 400 Bad Request\r\n".encode()
         except:
             return "HTTP/1.1 500 Internal Server Error".encode()
 
-    if path.startswith('\\calculate-area?'):
+    if resource.startswith('\\calculate-area?'):
         try:
-            area_value, length = area(path)
+            area_value, length = area(resource)
             return f"HTTP/1.1 200 OK\r\nContent-Length: {length}\r\nContent-Type: text/plain\r\n\r\n{area_value}".encode()
         except ValueError:
             return "HTTP/1.1 400 Bad Request\r\n".encode()    
         except:
             return "HTTP/1.1 500 Internal Server Error".encode()
     
-    if path.startswith('\\image?'):
+    if resource.startswith('\\image?'):
         try:
-            image_content, content_type, length = send_image(path)
+            image_content, content_type, length = send_image(resource)
             return f"HTTP/1.1 200 OK\r\nContent-Length: {length}\r\nContent-Type: {content_type}\r\n\r\n".encode() + image_content
         except IsADirectoryError:
             return "HTTP/1.1 404 Not Found".encode()
@@ -135,17 +151,17 @@ def GET(path):
         except:
             return "HTTP/1.1 500 Internal Server Error".encode()
 
-    if not path.endswith(('.html', '.jpg', '.js', '.css', '.ico', 'gif')):
-        path = path + '.html'
+    if not resource.endswith(('.html', '.jpg', '.js', '.css', '.ico', 'gif')):
+        resource = resource + '.html'
     
-    if path in FORBIDDEN:
+    if resource in FORBIDDEN:
         return "HTTP/1.1 403 Forbidden\r\n".encode()
     
-    if path in REDIRECT:
-        return "HTTP/1.1 302 Found\r\nLocation: {}\r\n".format(REDIRECT[path]).encode()
+    if resource in REDIRECT:
+        return "HTTP/1.1 302 Found\r\nLocation: {}\r\n".format(REDIRECT[resource]).encode()
     
     try:
-        file_content, file_type, length = get_file_data(path)
+        file_content, file_type, length = get_file_data(resource)
         if file_type == 'image/jpeg' or file_type == 'image/gif':
             return "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\n\r\n".format(length, file_type).encode() + file_content
         return "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\n\r\n{}".format(length, file_type, file_content).encode()
@@ -160,17 +176,20 @@ def GET(path):
     except:
         return "HTTP/1.1 500 Internal Server Error".encode()
 
-
-def POST(path, body):
-    if path in FORBIDDEN:
+#if the method is post check the resource
+#pass to the correct function with the body depend on the resource
+#from the function result create response if the function raised error
+#return http error code depend on the exception
+def POST(resource, body):
+    if resource in FORBIDDEN:
         return "HTTP/1.1 403 Forbidden\r\n".encode()
     
-    if path in REDIRECT:
-        return "HTTP/1.1 302 Found\r\nLocation: {}\r\n".format(REDIRECT[path]).encode()
+    if resource in REDIRECT:
+        return "HTTP/1.1 302 Found\r\nLocation: {}\r\n".format(REDIRECT[resource]).encode()
     
-    if path.startswith('\\upload'):
+    if resource.startswith('\\upload'):
         try:
-            file_name, image_length = post_image(path, body) 
+            file_name, image_length = post_image(resource, body) 
             return "HTTP/1.1 200 OK\r\n\r\nfile {} of size {} was saved successfully".format(file_name, image_length).encode()
         except ValueError:
             return "HTTP/1.1 400 Bad Request\r\n".encode()
@@ -179,16 +198,17 @@ def POST(path, body):
         except:
             return "HTTP/1.1 500 Internal Server Error".encode()
  
-
-def create_response(method, path, body):
-    path = path.replace('/', '\\')
-    if path == '\\':
-        path = '\\index.html'
+#get method resource and body(might be None)
+#pass to the correct function
+def create_response(method, resource, body):
+    resource = resource.replace('/', '\\')
+    if resource == '\\':
+        resource = '\\index.html'
 
     if method == "GET":
-        return GET(path)
+        return GET(resource)
     if method == "POST":
-        return POST(path, body)
+        return POST(resource, body)
 
 def handle_client(client_socket): 
     body = None
@@ -196,7 +216,7 @@ def handle_client(client_socket):
         try:
             request = client_socket.recv(2048).decode()
 
-            valid, method, path, content_length = check_request(request)
+            valid, method, resource, content_length = check_request(request)
             if valid:
 
                 if content_length != None:
@@ -204,7 +224,7 @@ def handle_client(client_socket):
                     while len(body) != content_length:
                         body = body + client_socket.recv(content_length)
                 
-                response = create_response(method, path, body)
+                response = create_response(method, resource, body)
                 client_socket.send(response)
                 break
             else:
